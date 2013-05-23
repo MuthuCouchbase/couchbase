cc4282b49f907067e594fc3ca89f6be553ec71f7
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-05-24 03:09:29 +0530

    Update Couchbase.php

--- Couchbase.php
+++ Couchbase.php
@@ -1,14 +1,14 @@
 <?php
 // adjust these parameters to match your installation
 $cb = new Couchbase("54.215.110.211:8091", "", "", "default");
-$booksHash = array();
+$arrayHash = array();
 $counter=1;
 while(true) {
    for($i=1;$i < 5001;$i++) {
-      $booksHash["CouchbaseKey".$i] = "Hi Buddy this is Couchbase";
+      $arrayHash["CouchbaseKey".$i] = "Hi Buddy this is Couchbase";
    }
    $cb->setMulti($booksHash, $expiry = 0, $persist_to = 0, $replicate_to = 0);
-   foreach ($booksHash as $key => $value) {
+   foreach ($arrayHash as $key => $value) {
       $cb->delete($key, $cas = "", $persist_to = 0, $replicate_to = 0);
    }
    var_dump("Setting and Deleting keys for the $counter time");
