fa2ea8a96bb51626b70edb5e05b3ac80a7b9ae9f
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-05-29 22:31:36 +0530

    Update CouchbaseNew.php

--- CouchbaseNew.php
+++ CouchbaseNew.php
@@ -3,6 +3,7 @@
 $cb = new Couchbase("54.241.219.191:8091", "", "", "default");
 $arrayHash = array();
 $counter=0;
+ini_set('memory_limit', '-1');
 $file_handle = fopen("name1.txt", "rb");
 while (!feof($file_handle) ) {
    $line_of_text = fgets($file_handle);
@@ -11,8 +12,9 @@ while (!feof($file_handle) ) {
 }
 fclose($file_handle);
 while(true) {
-   for ($i = 0; $i < count($arrayHash); ++$i) {
-      $temp_array = array($arrayHash[$i]);
+   global $arrayHash;
+   foreach($arrayHash as $key => $value) {
+      $temp_array[$key] = $value;
       $counter+=1;
       if($counter%5000 == 0) {
          $cb->setMulti($temp_array, $expiry = 0, $persist_to = 0, $replicate_to = 0);
