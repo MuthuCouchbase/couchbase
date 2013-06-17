73ea60307816bc3cd090962e06f29e32ccf765e7
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-06-18 04:48:00 +0530

    Update CouchbaseTroubleShoot.pl

--- CouchbaseTroubleShoot.pl
+++ CouchbaseTroubleShoot.pl
@@ -90,14 +90,14 @@ while(<FILE>) {
    if($_ =~ /\*\*|^[A-Za-z]+/ && $_ !~ /stats/) {
       print $_."\n";
    };
-   if($_ =~ / curr_items:|vb_active_curr_items:|_wat|mem_used|vb_replica_curr_items:|kv_size|bytes:/) {
+   if($_ =~ / curr_items:|vb_active_curr_items:|_wat|mem_used|vb_replica_curr_items:|kv_size| bytes:/) {
    my ($flag, $value) = split(/  +/, $_);
    my $key_meta_size = 54+70;
    if($flag =~ / (.+?items):/ ) {
      print $flag."\t$value\tcalculated_$1_size\t".int(($value*$key_meta_size)/(1024*1024*1024))." GB\n" if(length($value) > 10 && $value != "0");
      print $flag."\t$value\tcalculated_$1_size\t".int(($value*$key_meta_size)/(1024*1024))." MB\n" if(length($value) <= 10 && $value != "0");
    }
-   elsif($flag =~ /_wat|mem_used|kv_size|bytes:/) {
+   elsif($flag =~ /_wat|mem_used|kv_size| bytes:/) {
       print $flag."\t".int(($value)/(1024*1024*1024))." GB\n" if(length($value) > 10 && $value ne "0");
       print $flag."\t".int(($value)/(1024*1024))." MB\n" if(length($value) <= 10 && $value ne "0");
    }
