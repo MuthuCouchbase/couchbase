8bd694444ee8da7a2b7516ec7e0bd0a182079802
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-06-19 03:40:18 +0530

    Create BulkCouchbaseLogAnalyser.pl

---
+++ BulkCouchbaseLogAnalyser.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+opendir(DIR,".") or die "Couldn't open dir\n";
+my @files = grep("ADM-",readdir(DIR));                                                                                                                                  
+foreach(@files) {                                                                                                                                                       
+   system("unzip $_");                                                                                                                                                  
+}                                                                                                                                                                       
+my @cbfiles = grep(/cbcollect/,readdir(DIR));
+foreach(@cbfiles) {
+   chdir($_);
+   system("perl /trouble.pl > /output_$_.txt");
+   system("grep -E 'Port server memcached exited' ns_server.info.log >> /output_$_.txt");
+   system("grep -E 'Could not auto-failover node|was automatically failovered' diag.log >> /output_$_.txt");
+   system("perl /stats.pl --s3fileName=ns_server.debug.log");
+   system("mv OUTPUT_STATS.txt /OUTPUT_STATS.txt_$_.txt");
+   system("rm -rf $_");
+   chdir("..");
+}
+closedir(DIR) or warn "Couldn't close dir\n";
