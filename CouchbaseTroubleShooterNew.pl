388375bd8e7bce56cc1e81876482308f54c0fcdb
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-06-20 01:54:55 +0530

    Update CouchbaseTroubleShooterNew.pl

--- CouchbaseTroubleShooterNew.pl
+++ CouchbaseTroubleShooterNew.pl
@@ -97,7 +97,7 @@ while(<FILE>) {
       print $_."\n";
    };
    if($_ =~ / curr_items:|vb_active_curr_items:|_wat|mem_used|vb_replica_curr_items:|kv_size| bytes:|vb_replica_perc_mem_resident:|vb_active_perc_mem_resident:/) {
-   my ($flag, $value) = split(/  +/, $_);
+   my ($flag, $value) = split(/\s\s+/, $_);
    my $key_meta_size = 54+70;
    if($flag =~ / (.+?items):/ ) {
      print $flag."\t$value\tcalculated_$1_size\t".int(($value*$key_meta_size)/(1024*1024*1024))." GB\n" if(length($value) > 10 && $value != "0");
