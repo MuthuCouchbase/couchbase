7140d0d07180ae0089183b0ad776b941f2f08db4
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-06-22 01:43:23 +0530

    Update BulkCouchbaseLogAnalyser.pl

--- BulkCouchbaseLogAnalyser.pl
+++ BulkCouchbaseLogAnalyser.pl
@@ -4,7 +4,7 @@ use warnings;
 opendir(DIR,".") or die "Couldn't open dir\n";
 my @files = grep("zip",readdir(DIR));
 unless (-e "./CouchbaseTroubleShooterNew.pl" || -e "./CouchbaseStatsGenerator.pl") {
-   die "Copy the CouchbaseTroubleShooterNew.pl and CouchbaseStatsGenerator.pl to the current directory with the same name from github\n";
+   die "Copy the CouchbaseTroubleShooterNew.pl and CouchbaseStatsGenerator.pl to the / directory with the same name from github\n";
 }  
 foreach(@files) {                                                                                                                                                       
    system("unzip $_");                                                                                                                                                  
@@ -12,10 +12,10 @@ foreach(@files) {
 my @cbfiles = grep(/cbcollect/,readdir(DIR));
 foreach(@cbfiles) {
    chdir($_);
-   system("perl /trouble.pl > /output_$_.txt");
+   system("perl /CouchbaseTroubleShooterNew > /output_$_.txt");
    system("grep -E 'Port server memcached exited' ns_server.info.log >> /output_$_.txt");
    system("grep -E 'Could not auto-failover node|was automatically failovered' diag.log >> /output_$_.txt");
-   system("perl /stats.pl --s3fileName=ns_server.debug.log");
+   system("perl /CouchbaseStatsGenerator.pl --s3fileName=ns_server.debug.log");
    system("mv OUTPUT_STATS.txt /OUTPUT_STATS.txt_$_.txt");
    system("rm -rf $_");
    chdir("..");
