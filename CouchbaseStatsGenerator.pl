cd5ef738943a1c3ec2ec835dbe1c496325613d23
Muthukumar Koteeswaran <mkumar@couchbase.com>
2013-07-03 04:01:31 +0530

    Update CouchbaseStatsGenerator.pl

--- CouchbaseStatsGenerator.pl
+++ CouchbaseStatsGenerator.pl
@@ -11,8 +11,9 @@ GetOptions ("startDate=s"  => \$startDate,
             "node=s"       => \$node,
             "s3fileName=s" => \$s3fileName
            );
-if(!$s3fileName || $s3fileName !~ /ns_server.debug.log/) {
-   print "Usage perl CouchbaseStatsGenerator.pl --flags[optional] --s3fileName=ns_server.debug.log --startDate[optional] --endDate[optional] --node[optional]\n";
+if(!$s3fileName || $s3fileName !~ /ns_server.debug.log|ns_server.stats.log/) {
+   print "Usage perl CouchbaseStatsGenerator.pl --flags[optional] --s3fileName=ns_server.debug.log or ns_server.stats.log --startDate[optional] --endDate[optional] --node[optional]\n";
+   print "Please provide ns_server.stats.log only for 2.x CB\n";
    die "Please check the inputs\n";
 }
 $flags = "mem_used,curr_items,curr_items_tot,ep_diskqueue_drain,ep_diskqueue_fill,ep_diskqueue_items,ep_diskqueue_memory,ep_flusher_todo,ep_io_num_read,ep_io_num_write,ep_io_read_bytes,ep_io_write_bytes,ep_kv_size,ep_num_non_resident,ep_num_value_ejects,ep_queue_size,ep_tmp_oom_errors,ep_total_enqueued,ep_value_size,vb_active_curr_items,vb_active_eject,vb_active_itm_memory,vb_active_num_non_resident,vb_active_perc_mem_resident,vb_replica_curr_items,vb_replica_itm_memory,vb_replica_num_non_resident,vb_replica_perc_mem_resident" if(!$flags) ;
@@ -24,7 +25,7 @@ open(OUTPUTFILE,">","OUTPUT_STATS.txt") or die "Couldn't open file for writing\n
 print OUTPUTFILE "DATE,HOST,BUCKET_NAME,FLAG,VALUE\n";
 while(<FILE>) {
    chomp($_);
-   if($_ =~ / \[([^\]]+)\] \[ns_1@([^:]+):.*?"([^"]+)":$/) {
+   if($_ =~ / \[([^\]]+)\] \[ns_1@([^:]+):.*?"([^"]+)":$/ || $_ =~ /stats:debug,([^\.]+).*?,ns_1@([^:]+):.*?"([^"]+)":$/) {
       $date = $1;
       $host = $2;
       $bucket = $3;
